deaac1e03ab4adbd1964925835679a3988928848
Dwight Gunning <dwight@dwightgunning.com>
2019-09-06 18:05:18 +0200

    Added webpack-html-server and babel-loader to improve dev experience

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,28 @@
-module.exports = {
-  mode: 'development'
-};
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env) => ({
+  devtool: env.dev ? 'inline-source-map' : 'source-map',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Reactive Space Game',
+      template: './src/index.html'
+    })
+  ],
+  watch: env.dev
+});
